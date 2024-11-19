14ebf99476aab9b64ef2de5921c1c4182efde6e5
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-11-18 23:37:00 -0300

    Add loading state to LogIn component; display spinner and disable button during sign-in process

--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -10,6 +10,7 @@ import { LogInFormValues } from "../../types/forms";
 const LogIn = () => {
   const navigate = useNavigate();
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const {
     register,
@@ -20,12 +21,15 @@ const LogIn = () => {
   });
 
   const handleLogIn = handleSubmit(async (data) => {
+    setLoading(true);
     try {
       await signInWithEmailAndPassword(auth, data.email, data.password);
       navigate("/");
     } catch (error) {
       setError("Invalid email or password");
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   });
 
@@ -65,9 +69,36 @@ const LogIn = () => {
           {error && <p className="text-red-600 font-bold mb-4">{error}</p>}
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg font-bold hover:bg-blue-600 transition duration-300"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg font-bold hover:bg-blue-600 transition duration-300 disabled:opacity-50"
+            disabled={loading}
           >
-            Log In
+            {loading ? (
+              <div className="flex items-center justify-center">
+                <svg
+                  className="animate-spin h-5 w-5 mr-3 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                Signing In...
+              </div>
+            ) : (
+              "Log In"
+            )}
           </button>
         </form>
       </div>
